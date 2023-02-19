d31782663872f0830d1875e315a78e2ccc7fa9f8
Kathryn Carty <kcarty@bayviewglen.ca>
2023-02-19 16:47:52 -0500

    after lesson two

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script></script>
+
+<template>
+  <div>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+    <br />
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <a href="/about">A tag About</a>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style></style>
